70d8c50d31e129436ffe6a7aba9c29ad20485751
Specter2035 <142452789+Specter2035@users.noreply.github.com>
2023-10-28 09:53:36 -0600

    Add files via upload

---
+++ MetodoSimplexPrototipo/Form1.Designer.cs
@@ -0,0 +1,398 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtZx1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtZx2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtR1x1 = new System.Windows.Forms.TextBox();
+            this.txtR1x2 = new System.Windows.Forms.TextBox();
+            this.txtR1x3 = new System.Windows.Forms.TextBox();
+            this.txtR2x3 = new System.Windows.Forms.TextBox();
+            this.txtR2x2 = new System.Windows.Forms.TextBox();
+            this.txtR2x1 = new System.Windows.Forms.TextBox();
+            this.txtR3x3 = new System.Windows.Forms.TextBox();
+            this.txtR3x2 = new System.Windows.Forms.TextBox();
+            this.txtR3x1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label13 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(140, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Método Simplex";
+            // 
+            // txtZx1
+            // 
+            this.txtZx1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtZx1.Location = new System.Drawing.Point(82, 81);
+            this.txtZx1.Name = "txtZx1";
+            this.txtZx1.Size = new System.Drawing.Size(100, 26);
+            this.txtZx1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(51, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "z =";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(188, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "x1 +";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(338, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "x2";
+            // 
+            // txtZx2
+            // 
+            this.txtZx2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtZx2.Location = new System.Drawing.Point(232, 81);
+            this.txtZx2.Name = "txtZx2";
+            this.txtZx2.Size = new System.Drawing.Size(100, 26);
+            this.txtZx2.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(118, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Restricciones";
+            // 
+            // txtR1x1
+            // 
+            this.txtR1x1.Location = new System.Drawing.Point(18, 192);
+            this.txtR1x1.Name = "txtR1x1";
+            this.txtR1x1.Size = new System.Drawing.Size(100, 20);
+            this.txtR1x1.TabIndex = 7;
+            // 
+            // txtR1x2
+            // 
+            this.txtR1x2.Location = new System.Drawing.Point(165, 192);
+            this.txtR1x2.Name = "txtR1x2";
+            this.txtR1x2.Size = new System.Drawing.Size(100, 20);
+            this.txtR1x2.TabIndex = 8;
+            // 
+            // txtR1x3
+            // 
+            this.txtR1x3.Location = new System.Drawing.Point(326, 192);
+            this.txtR1x3.Name = "txtR1x3";
+            this.txtR1x3.Size = new System.Drawing.Size(100, 20);
+            this.txtR1x3.TabIndex = 9;
+            // 
+            // txtR2x3
+            // 
+            this.txtR2x3.Location = new System.Drawing.Point(326, 231);
+            this.txtR2x3.Name = "txtR2x3";
+            this.txtR2x3.Size = new System.Drawing.Size(100, 20);
+            this.txtR2x3.TabIndex = 12;
+            // 
+            // txtR2x2
+            // 
+            this.txtR2x2.Location = new System.Drawing.Point(165, 231);
+            this.txtR2x2.Name = "txtR2x2";
+            this.txtR2x2.Size = new System.Drawing.Size(100, 20);
+            this.txtR2x2.TabIndex = 11;
+            // 
+            // txtR2x1
+            // 
+            this.txtR2x1.Location = new System.Drawing.Point(18, 231);
+            this.txtR2x1.Name = "txtR2x1";
+            this.txtR2x1.Size = new System.Drawing.Size(100, 20);
+            this.txtR2x1.TabIndex = 10;
+            // 
+            // txtR3x3
+            // 
+            this.txtR3x3.Location = new System.Drawing.Point(326, 269);
+            this.txtR3x3.Name = "txtR3x3";
+            this.txtR3x3.Size = new System.Drawing.Size(100, 20);
+            this.txtR3x3.TabIndex = 15;
+            // 
+            // txtR3x2
+            // 
+            this.txtR3x2.Location = new System.Drawing.Point(165, 269);
+            this.txtR3x2.Name = "txtR3x2";
+            this.txtR3x2.Size = new System.Drawing.Size(100, 20);
+            this.txtR3x2.TabIndex = 14;
+            // 
+            // txtR3x1
+            // 
+            this.txtR3x1.Location = new System.Drawing.Point(18, 269);
+            this.txtR3x1.Name = "txtR3x1";
+            this.txtR3x1.Size = new System.Drawing.Size(100, 20);
+            this.txtR3x1.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(124, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 20);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "x1 +";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(273, 192);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 20);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "x2 <=";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(124, 229);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 20);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "x1 +";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(124, 267);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(38, 20);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "x1 +";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(271, 231);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(47, 20);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "x2 <=";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(271, 267);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(47, 20);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "x2 <=";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(168, 319);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(97, 24);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "x1,x2 >= 0";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(483, 24);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(161, 62);
+            this.btnCalcular.TabIndex = 24;
+            this.btnCalcular.Text = "Generar Matriz y renglón pivote";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(450, 121);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(662, 168);
+            this.dataGridView1.TabIndex = 25;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(73, 417);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(228, 62);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "Continuar para mostrar procedimiento >>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(450, 307);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(662, 172);
+            this.dataGridView2.TabIndex = 27;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Constantia", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(29, 538);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(415, 94);
+            this.listBox1.TabIndex = 28;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(29, 501);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(144, 20);
+            this.label13.TabIndex = 29;
+            this.label13.Text = "Nuevo Renglón Z";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(1139, 735);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtR3x3);
+            this.Controls.Add(this.txtR3x2);
+            this.Controls.Add(this.txtR3x1);
+            this.Controls.Add(this.txtR2x3);
+            this.Controls.Add(this.txtR2x2);
+            this.Controls.Add(this.txtR2x1);
+            this.Controls.Add(this.txtR1x3);
+            this.Controls.Add(this.txtR1x2);
+            this.Controls.Add(this.txtR1x1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtZx2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtZx1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtZx1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtZx2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtR1x1;
+        private System.Windows.Forms.TextBox txtR1x2;
+        private System.Windows.Forms.TextBox txtR1x3;
+        private System.Windows.Forms.TextBox txtR2x3;
+        private System.Windows.Forms.TextBox txtR2x2;
+        private System.Windows.Forms.TextBox txtR2x1;
+        private System.Windows.Forms.TextBox txtR3x3;
+        private System.Windows.Forms.TextBox txtR3x2;
+        private System.Windows.Forms.TextBox txtR3x1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ MetodoSimplexPrototipo/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static double[,] tablasimplex = new double[4, 7];
+        static int pos = 0;
+        static double[,] tablasimplex2;
+        static int pos1 = 0;
+        static Queue<string> ColaC1 = new Queue<string>();
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //Generar matriz
+          // static double[,] tablasimplex = new double[4,7];
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            //Agregar columnas
+            dataGridView1.Columns.Add("sf", "Solución Factible");
+            dataGridView1.Columns.Add("z", "z");
+            dataGridView1.Columns.Add("x1", "x1");
+            dataGridView1.Columns.Add("x2", "x2");
+            dataGridView1.Columns.Add("s1", "s1");
+            dataGridView1.Columns.Add("s2", "s2");
+            dataGridView1.Columns.Add("s3", "s3");
+            dataGridView1.Columns.Add("sl", "Solución");
+                
+        
+            //Renglon z
+            tablasimplex[0, 0] = 1;
+            tablasimplex[0, 1] = double.Parse(txtZx1.Text)* -1;
+            tablasimplex[0, 2] = double.Parse(txtZx2.Text)* -1;
+            tablasimplex[0, 3] = 0;
+            tablasimplex[0, 4] = 0;
+            tablasimplex[0, 5] = 0;
+            tablasimplex[0, 6] = 0;
+            
+
+            //Renglon s1
+            tablasimplex[1, 0] = 0;
+            tablasimplex[1, 1] = double.Parse(txtR1x1.Text);
+            tablasimplex[1, 2] = double.Parse(txtR1x2.Text);
+            tablasimplex[1, 3] = 1;
+            tablasimplex[1, 4] = 0;
+            tablasimplex[1, 5] = 0;
+            tablasimplex[1, 6] = double.Parse(txtR1x3.Text);
+            
+
+            //Renglon s2
+            tablasimplex[2, 0] = 0;
+            tablasimplex[2, 1] = double.Parse(txtR2x1.Text);
+            tablasimplex[2, 2] = double.Parse(txtR2x2.Text);
+            tablasimplex[2, 3] = 0;
+            tablasimplex[2, 4] = 1;
+            tablasimplex[2, 5] = 0;
+            tablasimplex[2, 6] = double.Parse(txtR2x3.Text);
+            
+
+            //Renglon s3
+            tablasimplex[3, 0] = 0;
+            tablasimplex[3, 1] = double.Parse(txtR3x1.Text);
+            tablasimplex[3, 2] = double.Parse(txtR3x2.Text);
+            tablasimplex[3, 3] = 0;
+            tablasimplex[3, 4] = 0;
+            tablasimplex[3, 5] = 1;
+            tablasimplex[3, 6] = double.Parse(txtR3x3.Text);
+            
+            //Agregar a dataGridView
+            for (int row = 0; row < tablasimplex.GetLength(0); row++)
+            {
+                DataGridViewRow dataGridViewRow = new DataGridViewRow();
+                dataGridViewRow.CreateCells(dataGridView1);                 
+
+                for (int col = 0; col < tablasimplex.GetLength(1); col++)
+                {
+                    dataGridViewRow.Cells[col+1].Value = tablasimplex[row, col].ToString();
+                }
+
+                dataGridView1.Rows.Add(dataGridViewRow);
+            }
+
+
+            //Agregar filas fijas de la primer columna
+            dataGridView1.Rows[0].Cells[0].Value = "z";
+            dataGridView1.Rows[1].Cells[0].Value = "s1";
+            dataGridView1.Rows[2].Cells[0].Value = "s2";
+            dataGridView1.Rows[3].Cells[0].Value = "s3";
+            
+            //Primer paso, encontrar columna pivote
+            double min = 0;
+            //int pos = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                if (tablasimplex[0,i] < min)
+                {
+                   min = tablasimplex[0,i];
+                   pos = i;
+                }
+                
+            }
+
+            //Segundo paso, encontrar renglon pivote
+            double min1 = 99999;
+            //int pos1 = 0;
+            double renglon1 = tablasimplex[1,6] / tablasimplex[1,1];
+            double renglon2 = tablasimplex[2,6] / tablasimplex[2,1];
+            double renglon3 = tablasimplex[3,6] / tablasimplex[3,1];
+
+            double[] renglones = new double[3];
+            renglones[0] = renglon1;
+            renglones[1] = renglon2;
+            renglones[2] = renglon3;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (renglones[i] < min1)
+                {
+                    min1 = renglones[i];
+                    pos1 = i+1;
+                }
+            }
+
+            //La coordenada del coeficiente que se dividirá entre todo lo demas (elemento pivote)
+            double rpivote = tablasimplex[pos1,pos]; 
+
+            //Cambiar color de la celda de elemento pivote
+            dataGridView1.Rows[pos1].Cells[pos+1].Style.BackColor = Color.Red;
+
+            // Crear una segunda matriz con los nuevos valores
+           // double[,] tablasimplex2;
+            tablasimplex2 = tablasimplex;
+
+            //Dividir el renglon pivote por elemento pivote
+            for (int i = 0; i < 7; i++)
+            {
+                tablasimplex2[pos1, i] = tablasimplex2[pos1, i] / rpivote;
+               
+            }
+            
+
+            //Calcular el nuevo renglon s1 con el renglon pivote
+            /* double E_Pivote1 = tablasimplex[0, pos] * -1;
+             for (int i = 0; i < 7; i++)
+             {
+                 tablasimplex2[0, i] = ((E_Pivote1)*(tablasimplex2[pos1,i]))+tablasimplex[0,i];
+             }*/
+
+            //Agregar columnas a nuevo datagridview
+            dataGridView2.Columns.Add("sf", "Solución Factible");
+            dataGridView2.Columns.Add("z", "z");
+            dataGridView2.Columns.Add("x1", "x1");
+            dataGridView2.Columns.Add("x2", "x2");
+            dataGridView2.Columns.Add("s1", "s1");
+            dataGridView2.Columns.Add("s2", "s2");
+            dataGridView2.Columns.Add("s3", "s3");
+            dataGridView2.Columns.Add("sl", "Solución");
+
+            //Mostrar en un nuevo datagridview
+            for (int row = 0; row < tablasimplex.GetLength(0); row++)
+            {
+                DataGridViewRow dataGridViewRow2 = new DataGridViewRow();
+                dataGridViewRow2.CreateCells(dataGridView2);
+
+                for (int col = 0; col < tablasimplex2.GetLength(1); col++)
+                {
+                    dataGridViewRow2.Cells[col + 1].Value = tablasimplex2[row, col].ToString();
+                }
+
+                dataGridView2.Rows.Add(dataGridViewRow2);
+            }
+
+            //Agregar filas fijas de la primer columna
+            dataGridView2.Rows[0].Cells[0].Value = "z";
+            dataGridView2.Rows[1].Cells[0].Value = "x1";
+            dataGridView2.Rows[2].Cells[0].Value = "s2";
+            dataGridView2.Rows[3].Cells[0].Value = "s3";
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double E_Pivote1 = tablasimplex[0, pos] * -1;
+            double Const = tablasimplex[0, pos];
+            double E_Pivote2 = tablasimplex[1, pos] * -1;
+            for (int i = 0; i < 7; i++)
+            {
+                tablasimplex2[0, i] = ((E_Pivote1) * (tablasimplex2[pos1, i])) + tablasimplex[0, i];
+                ColaC1.Enqueue("-(" + Const + ") * " + (tablasimplex2[pos1, i]) + "\n " +" + "+ tablasimplex[0, i] + "\n " + " = " +
+                    ((E_Pivote1) * (tablasimplex2[pos1, i])) + " + (" + tablasimplex[0, i]+ ")  = "+ tablasimplex2[0,i]+ "\n \n");
+                listBox1.DataSource = null;
+                listBox1 .DataSource = ColaC1.ToArray();
+               
+            }
+           
+        }
+        
+    }
+}
